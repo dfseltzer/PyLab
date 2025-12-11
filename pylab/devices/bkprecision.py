1dda1281f82d4d080fcc32788d8380f7ba65ec21
D Seltzer <d.seltzer@karmanplus.com>
2025-12-11 13:12:18 -0700

    fixed bk commands

--- pylab/devices/bkprecision.py
+++ pylab/devices/bkprecision.py
@@ -12,14 +12,14 @@ class BK8616(Load):
     command_map = {  # type: ignore
         "enabled": ("INP", "INP?"),
         "mode": ("FUNC", "FUNC?"),
-        "voltage": ("VOLT:ON", "MEAS:VOLT"),
-        "current": ("CURR:ON", "MEAS:CURR"),
+        "voltage": ("VOLT", "MEAS:VOLT"),
+        "current": ("CURR", "MEAS:CURR"),
         "power": ("POW", "MEAS:POW")
     } 
 
     def __init__(self, name, address, cnx_type="VISA", cmd_type="SCPI", **cnx_args) -> None:
         super().__init__(name, cnx_type, address, cmd_type, self.command_file, **cnx_args)
-        
+
     
 class BK9129B(Source):
     command_file = "SCPI_BK9129B"  # type: ignore
